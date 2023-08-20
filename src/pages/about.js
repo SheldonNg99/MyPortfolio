26afa655fe6652c25e685987bd8aea8690a6e005
Kai Jin <45513154+SheldonNg99@users.noreply.github.com>
2023-08-20 11:38:10 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -78,7 +78,7 @@ const about = () => {
                     
                     <div className="flex flex-col items-end justify-center xl:items-center">
                         <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                            <AnimatedNumbers value={2}/>+
+                            <AnimatedNumbers value={20}/>+
                         </span>
                         <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
                         xs:text-sm">satisfied clients</h2>
@@ -86,7 +86,7 @@ const about = () => {
 
                     <div className="flex flex-col items-end justify-center xl:items-center">
                         <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                            <AnimatedNumbers value={3}/>+
+                            <AnimatedNumbers value={50}/>+
                         </span>
                         <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
                         xs:text-sm">projects completed</h2>
