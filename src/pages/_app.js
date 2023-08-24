21247d36f736d32c57ca8400b3e5eac527b4cb8e
Sheldon Ng <45513154+SheldonNg99@users.noreply.github.com>
2023-08-24 08:40:35 +0800

    Add Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
 import {Montserrat} from "next/font/google"
 import Head from "next/head"
+import { Analytics } from '@vercel/analytics/react';
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
+        <Analytics />
         <NavBar></NavBar>
         <Component {...pageProps}/>
         <Footer/>
